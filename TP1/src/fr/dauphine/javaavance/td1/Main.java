de1dc8c6f88a4103f3c7f6921fdc4b9c49342494
haman <meryeme.haman.etu@univ-lille.fr>
2021-10-25 14:49:11 +0200

    Exo 3 : DONE, TP4 : DONE

---
+++ TP1/src/fr/dauphine/javaavance/td1/Main.java
@@ -0,0 +1,15 @@
+package fr.dauphine.javaavance.td1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+    public class Main {
+        public static void main(String[] args) {
+            Map<Point, Circle> m = new HashMap<>();
+            Point p1 = new Point(100,150);
+            Circle c1 = new Circle(p1,100);
+            m.put(p1, c1);
+            System.out.println(m.containsKey(p1));
+            System.out.println(m.containsKey(new Point(100,150)));
+        }
+}
---
+++ TP4/src/main/java/exo3/Point.java
@@ -0,0 +1,97 @@
+package main.java.exo3;
+
+/**
+ * Author : Meryeme HAMAN
+ */
+
+public class Point {
+	
+	private final int x;
+	private final int y;
+	private static int nbPoints;
+
+	/**
+	 * The first constructor of the class Point
+	 * @param px integer field
+	 * @param py integer field
+	 */
+	public Point(int px, int py) {
+		this.x=px;
+		this.y=py;
+		this.nbPoints++;
+	}
+
+	/**
+	 * The second constructor of the class Point
+	 * @param p2 a point instance of Point
+	 */
+	public Point(Point p2) {
+		this.x=p2.x;
+		this.y=p2.y;
+		this.nbPoints++;
+	}
+
+	/**
+	 * returns the value of x
+	 * @return x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * returns the value of y
+	 * @return y
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * describes the object Point
+	 * @return a string describing the Object  Point
+	 */
+	@Override
+	public String toString() {
+		return "("+this.x+","+this.y+")";
+	}
+
+	/**
+	 * checks if an object is a Point and has the same content as the current Point or not
+	 * @param obj a Point
+	 * @return true if the obj is equal to the point, false otherwise
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Point))
+			return false;
+		Point other = (Point) obj;
+		return (this.x==other.x) && (this.y==other.y);
+	}
+
+	/**
+	 * translates a point
+	 * @param dx
+	 * @param dy
+	 * @return a new Point that represents the translated point
+	 */
+	public Point translate(int dx, int dy){
+		 return new Point(x+dx,y+dy);
+	}
+
+	public int hashCode(){
+		return x^Integer.rotateLeft(y,16);
+	}
+
+	public static void main(String[] args) {
+		Point p1 = new Point(4,5);
+		Point p2 = p1;
+		Point p3 = new Point(4,5);
+		System.out.println(p1.x+" "+p1.y);
+		System.out.println(p1);
+
+	}
+
+}
