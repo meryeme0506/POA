9f5e428ba48d9609104399c057926d47bcc7581d
haman <meryeme.haman.etu@univ-lille.fr>
2021-12-15 10:57:45 +0100

    Orientation

--- Projet/InfinityLoop/InfinityLoop/src/main/java/fr/dauphine/JavaAvance/Components/Orientation.java
+++ Projet/InfinityLoop/InfinityLoop/src/main/java/fr/dauphine/JavaAvance/Components/Orientation.java
@@ -11,5 +11,34 @@ public enum Orientation {
 	/* Implement all the possible orientations and 
 	 *  required methods to rotate
 	 */
+	NORTH,EAST,SOUTH,WEST;
 
+	public static Orientation getOrifromValue(int orientationValue) {
+	if(orientationValue==0)
+		return Orientation.NORTH;
+	if(orientationValue==1)
+		return Orientation.EAST;
+	if(orientationValue==2)
+		return Orientation.SOUTH;
+	if(orientationValue==3)
+		return Orientation.WEST;
+	return null;
+	}
+
+	public Orientation turn90() {
+		switch(this) {
+			case NORTH:
+				return Orientation.EAST;
+
+			case EAST:
+				return Orientation.SOUTH;
+
+			case SOUTH:
+				return Orientation.WEST;
+
+			case WEST:
+				return Orientation.NORTH;
+		}
+		return null;
+	}
 }
--- Projet/InfinityLoop/InfinityLoop/src/main/java/fr/dauphine/JavaAvance/Components/PieceType.java
+++ Projet/InfinityLoop/InfinityLoop/src/main/java/fr/dauphine/JavaAvance/Components/PieceType.java
@@ -1,7 +1,8 @@
 package fr.dauphine.JavaAvance.Components;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.LinkedList;
 
 /**
@@ -10,7 +11,54 @@ import java.util.LinkedList;
  * 
  */
 public enum PieceType {
-	// Each Type has a number of connectors and a specific value
-	
+    VOID,ONECONN,BAR,TTYPE,FOURCONN,LTYPE;
 
+    public static PieceType getTypefromValue(int typeValue) {
+        if(typeValue==0)
+            return PieceType.VOID;
+        if(typeValue==1)
+            return PieceType.ONECONN;
+        if(typeValue==2)
+            return PieceType.BAR;
+        if(typeValue==3)
+            return PieceType.TTYPE;
+        if(typeValue==4)
+            return PieceType.FOURCONN;
+        if(typeValue==5)
+            return PieceType.LTYPE;
+        return null;
+    }
+
+
+
+
+    public LinkedList<Orientation> setConnectorsList(Orientation north) {
+
+    }
+
+    public ArrayList<Orientation> getListOfPossibleOri() {
+        return new ArrayList<Orientation>(EnumSet.allOf(Orientation.class));
+    }
+
+    public Orientation getOrientation(Orientation orientation) {
+        switch(this){
+            case FOURCONN,VOID -> {
+                return Orientation.NORTH;
+            }
+            case ONECONN,TTYPE, LTYPE -> {
+                return orientation;
+            }
+            case BAR -> {
+                switch(orientation){
+                    case NORTH,SOUTH -> {
+                        return Orientation.NORTH;
+                    }
+                    case EAST,WEST -> {
+                        return Orientation.EAST;
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
